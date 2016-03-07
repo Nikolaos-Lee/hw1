0875fe7f9f51dbdfc43f9515a6fdc9f988fdabbe
Nikolaos-Lee <Leo@liyaoyudeMacBook-Pro.local>
2016-03-07 11:27:36 +0800

    app modify

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-..
+var http=require('http');
+
+gttp.createServer((req,res)=>{
+    
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000,'127.0.0.1');
